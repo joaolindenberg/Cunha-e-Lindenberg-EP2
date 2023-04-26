fc980d5f23a85d2fecd51c80df6df95658c7f635
joaolindenberg <joaoplindenberg@gmail.com>
2023-04-26 15:06:03 -0300

    EP2

---
+++ EP2.py
@@ -0,0 +1,21 @@
+def define_posicoes(linha,coluna,orientacao,tamanho):
+    if orientacao not in ['vertical', 'horizontal']:
+        return []
+    else:
+        posicao = []
+        if orientacao == 'vertical':
+            for i in range(linha,linha+tamanho):
+                posicao.append([i,coluna])
+        else:
+            for i in range(coluna,coluna+tamanho):
+                posicao.append([linha,i])
+    return posicao
+
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    if nome_navio not in frota:
+        frota[nome_navio] = []
+        frota[nome_navio].append(define_posicoes(linha,coluna,orientacao,tamanho))
+    else:
+        frota[nome_navio].append(define_posicoes(linha,coluna,orientacao,tamanho))
+    return frota
