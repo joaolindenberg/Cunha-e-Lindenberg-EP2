cd67710248d6c0604ab349de83433c7c00f552f0
joaolindenberg <joaoplindenberg@gmail.com>
2023-05-03 22:53:35 -0300

    Rodout!

--- EP2.py
+++ EP2.py
@@ -78,36 +78,29 @@ quant_navio = {"porta-aviões":1,"navio-tanque":2,"contratorpedeiro":3,"submarin
 d = {1:'vertical', 2:'horizontal'}
 frota = {}
 tamanho = 4
-tabuleiro = [
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-]
-frota = {
-    "porta-aviões":[],
-    "navio-tanque":[],
-    "contratorpedeiro":[],
-    "submarino": [],
-}
 
 #Programa
-for i in tipos:
-    for j in range(0,quant_navio[i]):
-        linha = int(input('Linha: '))
-        coluna = int(input('coluna: '))
+for navio in tipos:
+    i = 0
+    Posicao_valida = True
+    temp_frota = frota
+    while i < quant_navio[navio]:
+        Posicao_valida = True
+        while Posicao_valida:
+            print('Insira as informações referentes ao navio {0} que possui tamanho {1}'.format(navio,tamanho))
+            linha = int(input('Linha: '))
+            coluna = int(input('Coluna: '))
+            
+            if navio != 'submarino':
+                vertical_horizontal = int(input('[1] Vertical [2] Horizontal: '))
+                orientacao = d[vertical_horizontal]
         
-        if i != 'submarino':
-            vertical_horizontal = int(input('[1] Vertical [2] Horizontal'))
-            vertical_horizontal = d[vertical_horizontal]
-
-        posicao = posicao_valida()
+            if posicao_valida(frota,linha,coluna,orientacao,tamanho) == True:
+                preenche_frota(frota, navio,linha,coluna,orientacao,tamanho)
+                Posicao_valida = False
+            else:
+                print('Esta posição não está válida!')
+        i+=1
     tamanho -= 1
 
 print(frota)
